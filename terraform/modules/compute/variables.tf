9ca559376d055d23ed4015a2a20be43af907189d
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 20:44:40 +0000

    Refactor variables.tf to add descriptions and clean up
    Added descriptions for create_vms and enable_docker_vm variables. Removed duplicate variable definitions and cleaned up the file.

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,60 +1,28 @@
 variable "create_vms" {
-  type = bool
-}
-
-variable "enable_docker_vm" {
-  type = bool
-}
-
-variable "rg_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "name_prefix" {
-  type = string
-}
-
-variable "subnet_id" {
-  type = string
-}
-
-variable "admin_username" {
-  type = string
-}
-
-variable "ssh_public_key" {
-  type = string
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-=======
->>>>>>> origin/dev
-}
-
-variable "create_vms" {
   description = "Create Jenkins VM"
   type        = bool
   default     = false
 }
 
-variable "location" {
-  type = string
+variable "enable_docker_vm" {
+  description = "Create Docker VM"
+  type        = bool
+  default     = false
 }
 
 variable "rg_name" {
   type = string
 }
 
-variable "subnet_id" {
+variable "location" {
   type = string
 }
 
 variable "name_prefix" {
+  type = string
+}
+
+variable "subnet_id" {
   type = string
 }
 
@@ -66,7 +34,3 @@ variable "admin_username" {
 variable "ssh_public_key" {
   type = string
 }
-<<<<<<< HEAD
->>>>>>> origin/main
-=======
->>>>>>> origin/dev
