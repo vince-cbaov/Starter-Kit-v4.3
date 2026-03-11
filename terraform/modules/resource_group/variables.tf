9a19615721dc6fd16c2dac9335d174e98e7c1d87
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 15:09:29 +0000

    Update variables.tf

--- terraform/modules/resource_group/variables.tf
+++ terraform/modules/resource_group/variables.tf
@@ -1,12 +1,7 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.name
-  location = var.location
+variable "name" {
+  type = string
 }
 
-output "name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "location" {
-  value = azurerm_resource_group.rg.location
+variable "location" {
+  type = string
 }
