d794e51c8c3f86492e42dde27e764bd41da3fd60
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 21:14:27 +0000

    Refactor resource group variables and add outputs
    Removed duplicate variable declarations and added outputs for resource group name and location.

--- terraform/modules/resource_group/variables.tf
+++ terraform/modules/resource_group/variables.tf
@@ -1,29 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 variable "name" {
   type = string
 }
 
 variable "location" {
   type = string
-=======
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
-=======
-variable "name" {
-  type = string
->>>>>>> 0b58b27ab7c0049cc96e4fa5d46b6d765ceee0e9
 }
 
-variable "location" {
-  type = string
+output "name" {
+  value = azurerm_resource_group.rg.name
 }
-<<<<<<< HEAD
 
 output "location" {
   value = azurerm_resource_group.rg.location
->>>>>>> origin/main
 }
-=======
->>>>>>> 0b58b27ab7c0049cc96e4fa5d46b6d765ceee0e9
