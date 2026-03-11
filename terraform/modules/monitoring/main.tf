eb26ff70c81c99899fc07109c44ece99df79ff89
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 15:06:01 +0000

    Update main.tf

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -2,9 +2,8 @@ resource "azurerm_log_analytics_workspace" "law" {
   name                = "${var.name_prefix}-law"
   location            = var.location
   resource_group_name = var.rg_name
-
-  sku                = "PerGB2018"
-  retention_in_days  = 30
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 output "law_id" {
