890753cab1b655e2a7ed01eebd7f92627b36817b
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 15:07:55 +0000

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -3,19 +3,14 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.rg_name
 
-  address_space = [
-    "10.10.0.0/16"
-  ]
+  address_space = ["10.10.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet_app" {
   name                 = "app"
   resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-
-  address_prefixes = [
-    "10.10.1.0/24"
-  ]
+  address_prefixes     = ["10.10.1.0/24"]
 }
 
 output "subnet_id" {
