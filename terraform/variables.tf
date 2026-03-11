c5f80d197936274859dbf2163472f1538963ce99
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 20:50:28 +0000

    Fix merge conflicts in variables.tf
    Resolve merge conflicts in variable definitions.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,38 +33,15 @@ variable "tenant_id" {
 
 variable "create_vms" {
   type    = bool
-<<<<<<< HEAD
-<<<<<<< HEAD
   default = false
-=======
-  default = true
->>>>>>> origin/main
-=======
-  default = false
->>>>>>> 0b58b27ab7c0049cc96e4fa5d46b6d765ceee0e9
 }
 
 variable "enable_docker_vm" {
   type    = bool
   default = true
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-<<<<<<< HEAD
-=======
-}
->>>>>>> origin/main
-=======
-=======
-}
->>>>>>> origin/dev
 
 variable "sp_object_id" {
   description = "Object ID of the service principal (principal_id) that runs Terraform"
   type        = string
-<<<<<<< HEAD
 }
->>>>>>> 0b58b27ab7c0049cc96e4fa5d46b6d765ceee0e9
-=======
-}
->>>>>>> origin/dev
