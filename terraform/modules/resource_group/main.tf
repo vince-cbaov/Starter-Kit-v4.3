c0c758a1b619af637dd488185c07313dc282457e
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-11 21:51:07 +0000

    Remove output variables from resource group module
    Removed output variables for resource group name and location.

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -2,11 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
 }
-
-output "name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "location" {
-  value = azurerm_resource_group.rg.location
-}
