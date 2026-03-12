6e296368f802bbb3fb8bafcf1f3732d3b383d088
vince-cbaov <vfalconer@hotmail.co.uk>
2026-03-12 23:02:49 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,29 @@ variable "enable_docker_vm" {
   type    = bool
   default = true
 }
+
+variable "ssh_public_key" {
+  type = string
+  description = "SSH public key for admin access"
+}
+
+variable "sp_app_id" {
+  type = string
+  description = "Service Principal Application (client) ID"
+}
+
+variable "sp_secret" {
+  type      = string
+  sensitive = true
+  description = "Service Principal client secret"
+}
+
+variable "tenant_id" {
+  type = string
+  description = "Azure AD Tenant ID"
+}
+
+variable "sp_object_id" {
+  type = string
+  description = "Object ID of the Service Principal"
+}
