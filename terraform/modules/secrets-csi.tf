23a8287d78abb6ecf059cf7225a19231ffff868b
CBAOV <vfalconer@hotmail.co.uk>
2026-04-12 04:17:52 +0100

    update project starter-kit-v9-11-04-2026

---
+++ terraform/modules/secrets-csi.tf
@@ -0,0 +1,17 @@
+data "azurerm_client_config" "current" {}
+
+resource "local_file" "secretproviderclass" {
+  filename = "${path.module}/../k8s/csi/secretproviderclass.yaml"
+
+  content = templatefile(
+    "${path.module}/../k8s/csi/secretproviderclass.yaml.tftpl",
+    {
+      client_id = azurerm_user_assigned_identity.workload.client_id
+      tenant_id = data.azurerm_client_config.current.tenant_id
+    }
+  )
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
