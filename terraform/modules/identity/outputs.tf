04466e6530f03fd9c68641a0b9603fba2afcc727
CBAOV <vfalconer@hotmail.co.uk>
2026-04-19 02:52:41 +0100

    Update of a few modules3

--- terraform/modules/identity/outputs.tf
+++ terraform/modules/identity/outputs.tf
@@ -13,3 +13,8 @@ output "client_id" {
 output "uami_id" {
   value = azurerm_user_assigned_identity.workload.id
 }
+
+output "workload_uami_id" {
+  description = "ID of the User Assigned Managed Identity for workload/Jenkins"
+  value      = azurerm_user_assigned_identity.workload.id
+}
