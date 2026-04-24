b20ac322c5c567a98f21c595fa9d22e0ce964dcd
CBAOV <vfalconer@hotmail.co.uk>
2026-04-24 23:37:14 +0100

    Update of few files5 24/04/26

--- terraform/modules/identity/main.tf
+++ terraform/modules/identity/main.tf
@@ -4,18 +4,23 @@ resource "azurerm_user_assigned_identity" "workload" {
   name                = "${var.name_prefix}-uami"
   resource_group_name = var.resource_group_name
   location            = var.location
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_federated_identity_credential" "myapp_fic" {
   name                      = "myapp-fic"
   audience                  = ["api://AzureADTokenExchange"]
   issuer                    = var.aks_oidc_issuer_url
-  subject                   = var.subject
+  #subject                   = var.subject
+  subject   = "system:serviceaccount:default:myapp-sa"
   user_assigned_identity_id = azurerm_user_assigned_identity.workload.id
 }
 
-resource "azurerm_role_assignment" "uami_subscription_reader" {
-  scope                = "/subscriptions/${var.subscription_id}"
-  role_definition_name = "Reader"
+resource "azurerm_role_assignment" "uami_kv_secrets_user" {
+  scope                = module.kv.kv_id
+  role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.workload.principal_id
 }
